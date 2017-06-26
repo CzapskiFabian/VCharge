869754aadeb23f2d57b5f3ef822928bb40b6cb9c
Fabian Czapski <czapskifabian@gmail.com>
2017-06-26 23:02:48 +0100

    delete old project

--- VCharge.WebClient/gulpfile.js
+++
@@ -1,7 +0,0 @@
-﻿/*
-This file is the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
-var gulp = require('gulp');
-
