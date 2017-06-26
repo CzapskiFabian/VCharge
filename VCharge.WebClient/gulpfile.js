2e9e16716c334b23539d342e3272f9614eec79d1
Fabian Czapski <czapskifabian@gmail.com>
2017-06-26 21:55:50 +0100

    minor fix

--- VCharge.WebClient/gulpfile.js
+++ VCharge.WebClient/gulpfile.js
@@ -4,8 +4,4 @@ Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
-var bower = require('gulp-bower');
 
-gulp.task('bower', function () {
-    return bower();
-});
