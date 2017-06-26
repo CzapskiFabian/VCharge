9c42ac7b9b582c573fe34fe55f08c9a787ebaf1e
Fabian Czapski <czapskifabian@gmail.com>
2017-06-26 23:02:13 +0100

    add simple chart

---
+++ VCharge.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VCharge.Services;
+
+namespace VCharge.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMeterReaderService _meterReader;
+
+        public HomeController(IMeterReaderService meterReader)
+        {
+            _meterReader = meterReader;
+        }
+        public IActionResult Index()
+        {
+            var summaries = _meterReader.GetMonthlySummaries();
+
+            return View(summaries.Value);
+        }
+
+    }
+}
